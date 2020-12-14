2c12176cc7c5040954d0a69380da66214147980e
KRAB <developer@kabhishek18.com>
2020-12-14 19:27:55 +0530

    Migration update

--- database/migrations/2020_12_14_131228_category.php
+++ database/migrations/2020_12_14_131228_category.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Category extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('category', function (Blueprint $table) {
             $table->id();
+            $table->integer('parent_id');
             $table->string('name');
-            $table->integer('phone');
-            $table->text('address');
+            $table->text('description');
             $table->text('image');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->enum('status', array('active', 'inactive'));
             $table->timestamps();
         });
     }
@@ -34,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('category');
     }
 }
--- database/migrations/2020_12_14_131643_product.php
+++ database/migrations/2020_12_14_131643_product.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Product extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('product', function (Blueprint $table) {
             $table->id();
+            $table->integer('cat_id');
+            $table->integer('subcat_id');
             $table->string('name');
-            $table->integer('phone');
-            $table->text('address');
+            $table->text('meta');
+            $table->text('slug');
+            $table->text('information');
+            $table->text('description');
+            $table->enum('status', array('active', 'inactive'));
             $table->text('image');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +35,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product');
     }
 }
+
